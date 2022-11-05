361853d315c35eddb5db37a08579305cfa662661
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-05 20:46:09 +0200

    Day-5
    learn some DOM

---
+++ Day-5/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".get button").click(function () {
+    // alert($(".get div").html());
+    // alert($(".get div").text());
+    // alert($(".get input").val());
+    // alert($(".get a").attr("class"));
+    // .html(function(i, c) { return i ot c})
+    // prop(false) == attr(undifiend)
+    // before, after
+    // append, prepend
+    // $("<span>Some thing</span>").appendTo("tagName");
+    // $("<span>Some thing</span>").prependTo("tagName");
+    // remove = remove from DOM, clear events,
+    // detach = remove from DOM, save events inside element
+    // empty() = remove child element from parent
+    // removeAttr("attr attr ...")
+    // .css({"width" : "200px"})
+    // .addClass("classname")
+    // .removeClass("classname")
+    // .toggleClass("classname", callbackfunc, switch(true = add class, false = remove class))
+    // .width(), .innerWidth(), .outerWidth(true)
+    // .height(), .innerHeight(), .outerHeight(true)
+    // .clone()
+  });
+});
