88434d4c168c543fbc1eda3cf09ab0a32cf86bc1
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-12 20:42:43 +0200

    add day-6
    end of html, css methods

---
+++ Day-6/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // offset         return  top(y) and left(x) from top the dom to the element;
+  // ofsetParent    parent must have postion style;
+  // postion        return top(y) and left(x) from the parent to child;
+  // scrollTop()    get and set with scrollTop(px);
+  // scrollLeft()   get and set with scrollLeft(px);
+  // $("newcontent inside tag").replaceAll(tags);
+  // $("tags").replaceWith(newcontent inside tag);
+  // selctor.wrap("<div></div>") move selector inside div;
+  // selctor.unwrap("<div></div>") move selector outside div;
+  // selector.wrapAll("<div></div>") all seclector has same name move inside div;
+  // selector.wrapInner("<div></div>") all content inside selector will move to div;
+});
